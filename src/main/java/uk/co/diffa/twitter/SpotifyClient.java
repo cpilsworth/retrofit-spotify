f9b63ddfe1809bbc9909f22cb3f9c59d419f59bb
Chris Pilsworth <cpilsworth@gmail.com>
2014-06-18 18:50:32 +0100

    Formatting

--- src/main/java/uk/co/diffa/twitter/SpotifyClient.java
+++ src/main/java/uk/co/diffa/twitter/SpotifyClient.java
@@ -9,6 +9,7 @@ public class SpotifyClient {
     private static final String NOW_ARTIST_ID = "4tNxq9NGKTKaX8OkZBLgf0";
 
     public static void main(String[] args) {
+
         final Paged<Album[]> pagedAlbums = SPOTIFY.getAlbums(NOW_ARTIST_ID);
 
         for (Album album : pagedAlbums.getItems()) {
